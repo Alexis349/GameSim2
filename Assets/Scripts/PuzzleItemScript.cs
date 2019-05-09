9a59964306b3dd963f5ec171d982e32242cd8d55
Kat <connorjernigan@msn.com>
2019-05-09 04:51:00 -0500

    Update
    Re-did several levels, creating multiple models for each level, and began working on lighting for the breakroom.

---
+++ Assets/Scripts/PuzzleItemScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleItemScript : MonoBehaviour
+{
+    public string textToDisplay;
+    private bool noteToggle = false;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                //display message
+                //if !noteToggle then noteToggle = true; Display textToDisplay;
+                print("Used note");
+                print(noteToggle);
+                noteToggle = !noteToggle;
+            }
+        }
+    }
+}
